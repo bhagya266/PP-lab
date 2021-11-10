c6f35ac39e074c6ade2734bc594603975a0d6e51
bhagya266 <85730381+bhagya266@users.noreply.github.com>
2021-11-10 10:53:49 +0530

    Update linear.py

--- linear.py
+++ linear.py
@@ -1,19 +1,19 @@
 
-def linear(a, elem):
-    if a[i] == elem:
+def linear_search(a, ele):
+    if a[i] == ele:
         pos = i + 1
         return pos
 
-    if a[i] >= elem:
+    if a[i] >= ele:
         return 0
 
-def withrec(n, a, elem):
+def recursion(n, a, ele):
     n = int(input("Enter number of elements "))
     a = []
     for i in range(n):
         a = []
         x = int(input("Enter the elements: "))
         a.append(x)
-    elem = int(input("Enter the element to be searched: "))
+    ele = int(input("Enter the element to be searched: "))
 
-print(linear(a, elem))
+print(linear(a, ele))
